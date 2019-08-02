64d285ef320b37fe1500c74b5447c032b763e4fe
duhu <muxiaoering@163.com>
2019-08-02 15:41:40 +0800

    日常学习和工作中，关于语音识别的笔记。

---
+++ asr-note.cmd
@@ -0,0 +1,15 @@
+
+del asr-note.pdf
+xelatex -no-pdf --interaction=nonstopmode asr-note
+bibtex asr-note
+xelatex -no-pdf --interaction=nonstopmode asr-note
+xelatex --interaction=nonstopmode asr-note
+
+@echo off
+del /q *.aux chapters\*.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex(busy)
+
+
+del /q *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk 
+
+start "" "asr-note.pdf"
+
