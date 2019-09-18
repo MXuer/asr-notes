fcf228fcdd7bf1ab2edf99e869a4b9d4339a25c4
duhu <muxiaoering@163.com>
2019-09-18 09:46:25 +0800

    notes for edx

--- asr-note.cmd
+++ asr-note.cmd
@@ -6,7 +6,7 @@ xelatex -no-pdf --interaction=nonstopmode asr-note
 xelatex --interaction=nonstopmode asr-note
 
 @echo off
-del /q *.aux chapters\*.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex(busy)
+del /q *.aux chapters\*.aux *.bbl *.blg  *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex(busy)
 
 
 del /q *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk 
